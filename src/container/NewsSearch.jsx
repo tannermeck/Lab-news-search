6dbc9c31c55934e126400419940b53a4debeda52
TANNER MECK <steve.meck246@gmail.com>
2021-11-03 14:25:07 -0700

    newssearch container working

---
+++ src/container/NewsSearch.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewsSearch extends Component {
+    state = {}
+    render(){
+        return (
+            <h1>Hello from NewsSearch</h1>
+        )
+    }
+}
+
+export default NewsSearch;
